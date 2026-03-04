f1ed05fa39369da68369893a8a0e62e2b0870a44
Salvador Bravo <sa.bravo0901@gmail.com>
2026-03-04 20:21:58 -0300

    feat: agregar archivo de configuración con rutas de base de datos y modelos

---
+++ src/config.py
@@ -0,0 +1,4 @@
+CHROMADB_PATH = "chroma_db" # Ruta donde se guardará la base de datos de ChromaDB (vectores + metadatos)
+DOCS_PATH = "C:\\Users\\salva\\Desktop\\Development\\rag_support_ticket_langgraph\\src\\docs"
+
+EMBEDDING_MODEL = "models/gemini-embedding-001" # Modelo de embeddings de Google (768 dimensiones) - captura el significado semántico del texto
